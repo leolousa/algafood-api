7bc92253db37f83cdfe25508ab193c1a4eb863ab
leolousa <leolousa@hotmail.com>
2021-06-20 16:43:18 -0300

    Estendendo Problem Details p/ adicionar prop. com constraints violadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -113,9 +114,19 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
 	    ProblemType problemType = ProblemType.DADOS_INVALIDOS;
 	    String detail = "Um ou mais campos estão inválidos. Faça o preenchimento correto e tente novamente.";
-	        
+	    
+	    BindingResult bindingResult = ex.getBindingResult();
+	    
+	    List<Problem.Field> problemFields = bindingResult.getFieldErrors().stream()
+	    		.map(fieldError -> Problem.Field.builder()
+	    				.name(fieldError.getField())
+	    				.userMessage(fieldError.getDefaultMessage())
+	    				.build())
+	    		.collect(Collectors.toList());
+	    
 	    Problem problem = createProblemBuilder(status, problemType, detail)
 	        .userMessage(detail)
+	        .fields(problemFields)
 	        .build();
 	    
 	    return handleExceptionInternal(ex, problem, headers, status, request);
@@ -240,6 +251,8 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 	// O método joinPath foi feito para reaproveitar em todos os métodos
 	// que precisam concatenar os nomes das propriedades (separando por ".")
 	private String joinPath(List<Reference> references) {
-		return references.stream().map(ref -> ref.getFieldName()).collect(Collectors.joining("."));
+		return references.stream()
+				.map(ref -> ref.getFieldName())
+				.collect(Collectors.joining("."));
 	}
 }
