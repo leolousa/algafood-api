6232b28c8b5c5d0048d5d7f830b370e54f7529e2
leolousa <leolousa@hotmail.com>
2021-07-07 11:35:57 -0300

    Escrevendo testes de integração

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -2,6 +2,8 @@ package com.algaworks.algafood;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.math.BigDecimal;
+
 import javax.validation.ConstraintViolationException;
 
 import org.junit.jupiter.api.Assertions;
@@ -9,8 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import com.algaworks.algafood.domain.service.CadastroRestauranteService;
+
 /**
  * Classe de testes de integração
  * 
@@ -22,6 +29,9 @@ class CadastroCozinhaIntegrationTests {
 
 	@Autowired
 	private CadastroCozinhaService cadastroCozinha;
+	
+	@Autowired
+	private CadastroRestauranteService cadastroRestaurante;
 
 	@Test
 	public void deveAtribuirId_quandoCadastrarCozinhaComDadosCorretos() {
@@ -42,10 +52,39 @@ class CadastroCozinhaIntegrationTests {
 		Cozinha novaCozinha = new Cozinha();
 		novaCozinha.setNome(null);
 
-		ConstraintViolationException erroEsperado =
-			Assertions.assertThrows(ConstraintViolationException.class, () -> {
-				cadastroCozinha.salvar(novaCozinha);
-			});
+		ConstraintViolationException erroEsperado = Assertions.assertThrows(ConstraintViolationException.class, () -> {
+			cadastroCozinha.salvar(novaCozinha);
+		});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaEmUso() {
+		Cozinha cozinha = new Cozinha();
+		cozinha.setNome("Coreana");
+		cozinha = cadastroCozinha.salvar(cozinha);
+
+		Restaurante restaurante = new Restaurante();
+		restaurante.setNome("Restaurante coreano");
+		restaurante.setTaxaFrete(BigDecimal.TEN);
+		restaurante.setCozinha(cozinha);
+		restaurante = cadastroRestaurante.salvar(restaurante);
+
+		Long cozinhaEmUsoId = cozinha.getId();
+
+		EntidadeEmUsoException erroEsperado = Assertions.assertThrows(EntidadeEmUsoException.class, () -> {
+			cadastroCozinha.excluir(cozinhaEmUsoId);
+		});
+
+		assertThat(erroEsperado).isNotNull();
+	}
+
+	@Test
+	public void deveFalhar_QuandoExcluirCozinhaInexistente() {
+		CozinhaNaoEncontradaException erroEsperado = Assertions.assertThrows(CozinhaNaoEncontradaException.class, () -> {
+			cadastroCozinha.excluir(100L);
+		});
 
 		assertThat(erroEsperado).isNotNull();
 	}
