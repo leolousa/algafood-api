102391c9e1a54a2d53cc223a66623b617df684a8
leolousa <leolousa@hotmail.com>
2021-07-07 22:45:26 -0300

    Configurando um banco de testes e usando @TestPropertySource

--- src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -1,10 +1,14 @@
 package com.algaworks.algafood;
 
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
@@ -17,20 +21,30 @@ import io.restassured.http.ContentType;
  *
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 class CadastroCozinhaIT {
 	
 	@LocalServerPort
 	private int port;
+	
+	@Autowired
+	private Flyway flyway;
+
+	@BeforeEach //Método de preparação para os testes. Executado antes de cada teste
+	public void setUp() {
+		//Habilita o log da requisição caso o teste falhe
+		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+		RestAssured.port = port;
+		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate(); //Roda a migração antes de cada teste garantindo a massa de dados do afterMigrate.sql 
+	}
 
 	@Test
 	public void deveRetornarStatus200_quandoConsultarCozinhas() {
-		//Habilita o log da requisição caso o teste falhe
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		
 		//Faz a requisição para testar
 		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
@@ -41,12 +55,9 @@ class CadastroCozinhaIT {
 	
 	@Test
 	public void deveConter4Cozinhas_quandoConsultarCozinhas() {
-		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		
 		//Faz a requisição para testar
 		RestAssured.given()
-			.basePath("/cozinhas")
-			.port(port)
 			.accept(ContentType.JSON)
 		.when()
 			.get()
@@ -55,5 +66,17 @@ class CadastroCozinhaIT {
 			.body("nome", Matchers.hasItems("Indiana", "Tailandesa"));//Testa o retorno dos nomes nos objetos no array JSON
 		
 			
+	}
+	
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		RestAssured.given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
 	}
 }
