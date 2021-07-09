225f3d74e0179826880e86a5b32bbbfed8950767
leolousa <leolousa@hotmail.com>
2021-07-09 11:16:32 -0300

    Implementando a conversão de Entidade para DTO em Restaurante

---
+++ src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Classe DTO de representação do recurso para Cozinha
+ * 
+ * @author Leonardo
+ *
+ */
+@Getter
+@Setter
+public class CozinhaModel {
+
+	private Long id;
+	private String nome;
+	
+}
---
+++ src/main/java/com/algaworks/algafood/api/model/RestauranteModel.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.api.model;
+
+import java.math.BigDecimal;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Classe DTO de representação do recurso para Restaurante
+ * 
+ * @author Leonardo
+ *
+ */
+@Getter
+@Setter
+public class RestauranteModel {
+
+	private Long id;
+	private String nome;
+	private BigDecimal taxaFrete;
+	private CozinhaModel cozinha;
+	
+}
