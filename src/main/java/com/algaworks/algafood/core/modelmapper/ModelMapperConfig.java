f071f99e26245cb9c4a2fe2bc7ad19300bb219d2
leolousa <leolousa@hotmail.com>
2021-07-10 20:31:32 -0300

    Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -4,6 +4,9 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.algaworks.algafood.api.model.RestauranteModel;
+import com.algaworks.algafood.domain.model.Restaurante;
+
 /**
  * Classe de configuração do ModelMapper
  * para que o Spring possa gerenciar a sua injeção nas classes
@@ -17,5 +20,11 @@ public class ModelMapperConfig {
 
 	@Bean
 	public ModelMapper modelMapper() {
-		return new ModelMapper();	}
+		var modelMapper = new ModelMapper();
+		
+		modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+			.addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+		
+		return modelMapper;
+	}
 }
