e41bc6e81bc6341546b71303e57ec6e08b5f51c7
leolousa <leolousa@hotmail.com>
2021-07-13 01:41:41 -0300

    Criando migration para alterar propriedade do campo e-mail em Usuario

--- src/main/java/com/algaworks/algafood/domain/repository/UsuarioRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/UsuarioRepository.java
@@ -2,6 +2,7 @@ package com.algaworks.algafood.domain.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Usuario;
@@ -11,4 +12,7 @@ public interface UsuarioRepository extends CustomJpaRepository<Usuario, Long> {
 
 	Optional<Usuario> findByEmail(String email);
 	
+	// Implementando uma busca por e-mails duplicados
+	@Query("SELECT COUNT(u) > 0 FROM Usuario u WHERE u.email = :email and (:id is null or u.id <> :id)")
+    boolean existsByEmailAndId(String email, Long id);
 }
--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -1,7 +1,5 @@
 package com.algaworks.algafood.domain.service;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,16 +17,22 @@ public class CadastroUsuarioService {
     
     @Transactional
     public Usuario salvar(Usuario usuario) {
+    	// IMPLEMENTAÇÃO Thiago
     	//Remove o usuário do contexto de persistência 
     	//para evitar que o JPA gerencie o objeto usuário
-    	usuarioRepository.detach(usuario);
+    	//usuarioRepository.detach(usuario);
     	
-    	Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+    	//Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
     	
-    	if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
-    		throw new NegocioException(
-    				String.format("Já existe usuário cadastrado com o e-mail %s", usuario.getEmail()));
-    	}
+    	//if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+    		//throw new NegocioException(
+    				//String.format("Já existe usuário cadastrado com o e-mail %s", usuario.getEmail()));
+    	//}
+    	
+    	// IMPLEMENTAÇÃO método customizado
+    	if (usuarioRepository.existsByEmailAndId(usuario.getEmail(), usuario.getId())) {
+            throw new NegocioException(String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
+        }
     	
         return usuarioRepository.save(usuario);
     }
