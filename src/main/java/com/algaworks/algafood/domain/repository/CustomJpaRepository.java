56d004a9e736594b248aea07a77fedb35a1cbd03
leolousa <leolousa@hotmail.com>
2021-07-13 01:22:23 -0300

    Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CustomJpaRepository.java
@@ -17,4 +17,6 @@ import org.springframework.data.repository.NoRepositoryBean;
 public interface CustomJpaRepository<T, ID> extends JpaRepository<T, ID>{
 
 	Optional<T> buscarPrimeiro();
+	
+	void detach(T entity);
 }
