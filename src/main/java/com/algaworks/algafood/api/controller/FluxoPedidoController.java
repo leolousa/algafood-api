91d2e8c0c02fe2dea1bc02a21d888e5adc21b3d4
leolousa <leolousa@hotmail.com>
2021-08-06 10:54:35 -0300

    implementando endpoints de transição de status de pedidos (Confirmado, Cancelado e Entregue)

--- src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
+++ src/main/java/com/algaworks/algafood/api/controller/FluxoPedidoController.java
@@ -22,5 +22,17 @@ public class FluxoPedidoController {
     public void confimar(@PathVariable Long pedidoId) {
     	fluxoPedido.confirmar(pedidoId);
     }
+	
+	@PutMapping("/cancelamento")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void cancelar(@PathVariable Long pedidoId) {
+	    fluxoPedido.cancelar(pedidoId);
+	}
+	
+	@PutMapping("/entrega")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void entregar(@PathVariable Long pedidoId) {
+	    fluxoPedido.entregar(pedidoId);
+	}
     
 }
