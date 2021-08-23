7e2ddb83faee6899a7351233645988e22f7ab4ba
leolousa <leolousa@hotmail.com>
2021-08-23 18:59:35 -0300

    Gerando a documentação da API em HTML com Swagger UI e SpringFox

---
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.core.openapi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+/**
+ * Classe de configuração para o uso
+ * da OpenAPI 2 para documentação da API
+ * 
+ * @author Leonardo
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SpringFoxConfig {
+
+	@Bean
+	public Docket apiDockey() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+					.apis(RequestHandlerSelectors.any())
+					.build();
+	}
+}
