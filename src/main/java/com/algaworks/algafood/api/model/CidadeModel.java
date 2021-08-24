2da411bc10b162d6c756c529077109815571fb3d
leolousa <leolousa@hotmail.com>
2021-08-24 12:16:15 -0300

    Descrevendo modelos de representações e suas propriedades

--- src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CidadeModel.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,11 +11,18 @@ import lombok.Setter;
  * @author Leonardo
  *
  */
+//Muda o nome do modelo na documentação
+@ApiModel(value = "Cidade", description = "Representa uma cidade")
 @Getter
 @Setter
 public class CidadeModel {
 
+	//@ApiModelProperty(value = "ID da cidade", example = "1")
+	@ApiModelProperty(example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "Brasília")
 	private String nome;
+	
 	private EstadoModel estado;
 }
