2536073ec6593ea47c1e60719e5768cbc9e7dab1
leolousa <leolousa@hotmail.com>
2021-08-25 11:35:30 -0300

    Descrevendo o modelo de representação de Problem

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -6,28 +6,49 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Getter;
 
+@ApiModel("Problema")
 @JsonInclude(Include.NON_NULL)
 @Getter
 @Builder
 public class Problem {
 
+	@ApiModelProperty(example = "400", position = 1)
 	private Integer status;
+	
+	@ApiModelProperty(example = "2021-08-25T14:19:30Z", value = "Data e hora no formato ISO", position = 5)
+	private OffsetDateTime timeStamp;
+
+	@ApiModelProperty(example = "https://algafood.com.br/erro-negocio", position = 10)
 	private String type;
+	
+	@ApiModelProperty(example = "Violação de regra de negócio", position = 15)
 	private String title;
+	
+	@ApiModelProperty(example = "Não existe um cadastro de restaurante com código 10", position = 20)
 	private String detail;
 	
+	@ApiModelProperty(example = "Não existe um cadastro de restaurante com código 10", position = 25)
 	private String userMessage;
-	private List<Object> objects;
-	private OffsetDateTime timeStamp;
 	
+	@ApiModelProperty(value = "Lista de objetos ou campos que geraram o erro (opicional)",
+			position = 30)
+	private List<Object> objects;
+	
+	
+	@ApiModel("ObjetoProblema")
 	@Getter
 	@Builder
 	public static class Object {
 		
+		@ApiModelProperty(example = "Preço")
 		private String name;
+		
+		@ApiModelProperty(example = "O preço é obrigatório")
 		private String userMessage;
 		
 	}
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.algaworks.algafood.api.exceptionhandler.Problem;
+import com.fasterxml.classmate.TypeResolver;
+
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -37,6 +40,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
 	@Bean
 	public Docket apiDockey() {
+		var typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api"))
@@ -44,10 +49,11 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 //					.paths(PathSelectors.ant("/restaurantes/*")) //Especificar um end point específico
 					.build()
 					.useDefaultResponseMessages(false) // Mensagens default das respostas do endpoint
-					.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
+					.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages()) // Mapeamento dos métodos específicos e seus retornos
 					.globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
 		            .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
 		            .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
+		            .additionalModels(typeResolver.resolve(Problem.class)) //Lista um modelo extra na parte de Models da documentação
 				.apiInfo(apiInfo()) //Chama o método apiInfo para montar o cabeçalho da documentação
 				.tags(tags()[0], tags());//Chama o método tags para alterar os nomes dos end points na documentacao
 	}
