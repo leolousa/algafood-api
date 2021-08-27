73c25c54b2c6e52dc3553a88b673c9f87fee865e
leolousa <leolousa@hotmail.com>
2021-08-27 09:09:58 -0300

    Descrevendo documentação de endpoints de cozinhas

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -25,6 +25,7 @@ import com.algaworks.algafood.api.assembler.CozinhaInputDisassembler;
 import com.algaworks.algafood.api.assembler.CozinhaModelAssembler;
 import com.algaworks.algafood.api.model.CozinhaModel;
 import com.algaworks.algafood.api.model.input.CozinhaInput;
+import com.algaworks.algafood.api.openapi.controller.CozinhaControllerOpenApi;
 import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 import com.algaworks.algafood.domain.service.CadastroCozinhaService;
@@ -36,7 +37,7 @@ import io.swagger.annotations.ApiParam;
 @Api(tags = "Cozinhas")
 @RestController
 @RequestMapping(path = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
-public class CozinhaController {
+public class CozinhaController implements CozinhaControllerOpenApi {
 
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
--- src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
@@ -1,5 +1,6 @@
 package com.algaworks.algafood.api.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +13,11 @@ import lombok.Setter;
 @Getter
 @Setter
 public class CozinhaModel {
-
+	
+	@ApiModelProperty(example = "1")
 	private Long id;
+	
+	@ApiModelProperty(example = "Brasileira")
 	private String nome;
 	
 }
