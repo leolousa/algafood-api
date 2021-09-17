872abaac8e6087bb499cf9aeadcaa34804a03cbf
leolousa <leolousa@hotmail.com>
2021-09-17 10:57:30 -0300

    Configurando Cors no Resource Server

--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -20,6 +20,7 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 			.authorizeRequests()
 				.anyRequest().authenticated()
 			.and()
+			.cors().and()
 			.oauth2ResourceServer().opaqueToken();
 	}
 	
