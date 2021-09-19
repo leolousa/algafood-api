4fad4e943abf5ae3e0be7055ec1f2151cd025ff4
leolousa <leolousa@hotmail.com>
2021-09-19 17:29:33 -0300

    Alterando as senhas para hashcode para testes

--- src/main/resources/db/dados-testes/afterMigrate.sql
+++ src/main/resources/db/dados-testes/afterMigrate.sql
@@ -83,13 +83,13 @@ insert into grupo (id, nome) values (1, 'Gerente'), (2, 'Vendedor'), (3, 'Secret
 insert into grupo_permissao (grupo_id, permissao_id) values (1, 1), (1, 2), (2, 1), (2, 2), (3, 1); 
 
 insert into usuario (id, nome, email, senha, data_cadastro) values
-(1, 'João da Silva', 'joao.ger@algafood.com', '123', utc_timestamp),
-(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '123', utc_timestamp),
-(3, 'José Souza', 'jose.aux@algafood.com', '123', utc_timestamp),
-(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '123', utc_timestamp),
-(5, 'Manoel Lima', 'manoel.loja@gmail.com', '123', utc_timestamp),
-(6, 'Débora Mendonça', 'clinicafundamento+debora@gmail.com', '123', utc_timestamp),
-(7, 'Carlos Lima', 'clinicafundamento+carlos@gmail.com', '123', utc_timestamp);
+(1, 'João da Silva', 'joao.ger@algafood.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp),
+(2, 'Maria Joaquina', 'maria.vnd@algafood.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp),
+(3, 'José Souza', 'jose.aux@algafood.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp),
+(4, 'Sebastião Martins', 'sebastiao.cad@algafood.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp),
+(5, 'Manoel Lima', 'manoel.loja@gmail.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp),
+(6, 'Débora Mendonça', 'clinicafundamento+debora@gmail.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp),
+(7, 'Carlos Lima', 'clinicafundamento+carlos@gmail.com', '$2a$12$Sgax7zSBZn1j2H93jUNwpOs13x.3oBgttbG45q7LVGE7jVJHF1reu', utc_timestamp);
 
 insert into usuario_grupo (usuario_id, grupo_id) values (1, 1), (1, 2), (2, 2);
 
