652c47d6bf1f04f2a1391cd1aa35a4b253e2fef4
leolousa <leolousa@hotmail.com>
2021-09-22 11:54:51 -0300

    Method Security: Restringindo acesso com @PreAuthorize e SpEL

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,9 +33,8 @@ import com.algaworks.algafood.domain.service.CadastroCozinhaService;
 
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import lombok.extern.slf4j.Slf4j;
 
-@Slf4j
+//@Slf4j
 @RestController
 @RequestMapping(path = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CozinhaController implements CozinhaControllerOpenApi {
@@ -54,6 +54,7 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	@Autowired
 	private CozinhaInputDisassembler cozinhaInputDisassembler;  
 
+	@PreAuthorize("isAuthenticated()")
 	@ApiOperation("Lista as cozinhas")
 	@GetMapping
 	public Page<CozinhaModel> listar(@PageableDefault(size = 10) Pageable pageable) {
@@ -69,6 +70,7 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	    return cozinhasModelPage;
 	}
 
+	@PreAuthorize("isAuthenticated()")
 	@ApiOperation("Busca uma cozinha por ID")
 	@GetMapping("/{cozinhaId}")
 	public CozinhaModel buscar(
@@ -79,6 +81,7 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	    return cozinhaModelAssembler.toModel(cozinha);
 	}
 
+	@PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
 	@ApiOperation("Cadastra uma nova cozinha")
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
@@ -90,6 +93,7 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	    return cozinhaModelAssembler.toModel(cozinha);
 	}
 
+	@PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
 	@ApiOperation("Atualiza uma cozinha por ID")
 	@PutMapping("/{cozinhaId}")
 	public CozinhaModel atualizar(
@@ -103,6 +107,7 @@ public class CozinhaController implements CozinhaControllerOpenApi {
 	    return cozinhaModelAssembler.toModel(cozinhaAtual);
 	}
 
+	@PreAuthorize("hasAuthority('EDITAR_COZINHAS')")
 	@ApiOperation("Exclui uma cozinha por ID")
 	@DeleteMapping("/{cozinhaId}")
 	@ResponseStatus(HttpStatus.NO_CONTENT)
--- src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
+++ src/main/java/com/algaworks/algafood/core/security/ResourceServerConfig.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -20,17 +21,13 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
-			.authorizeRequests()
-				.antMatchers(HttpMethod.POST, "/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
-				.antMatchers(HttpMethod.PUT, "/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
-				.antMatchers(HttpMethod.GET, "/cozinhas/**").authenticated()
-				.anyRequest().denyAll()
-			.and()
+			.csrf().disable()
 			.cors().and()
 			.oauth2ResourceServer()
 				.jwt()
