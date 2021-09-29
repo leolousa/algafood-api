071ccee8d5b23598b7246b6f72cf25ba5db48424
leolousa <leolousa@hotmail.com>
2021-09-29 12:13:32 -0300

    Implementando o endpoint do JSON Web Key Set (JWKS)

---
+++ src/main/java/com/algaworks/algafood/core/security/authorizationserver/JwkSetController.java
@@ -0,0 +1,28 @@
+package com.algaworks.algafood.core.security.authorizationserver;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nimbusds.jose.jwk.JWKSet;
+
+/**
+ * Classe Controller que expõe o Endpoint
+ * do JWK
+ * 
+ * @author Leonardo
+ *
+ */
+@RestController
+public class JwkSetController {
+
+	@Autowired
+	private JWKSet jwkSet;
+	
+	@GetMapping("/.well-known/jwks.json")
+	public Map<String, Object> keys() {
+		return jwkSet.toJSONObject();
+	}
+}
