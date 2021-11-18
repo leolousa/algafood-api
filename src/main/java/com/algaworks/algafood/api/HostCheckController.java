c33b4ca4ea8cba91b0f101fc56849531cda23a28
leolousa <leolousa@hotmail.com>
2021-11-17 21:03:37 -0300

    Configurando um proxy reverso com Nginx

--- src/main/java/com/algaworks/algafood/api/HostCheckController.java
+++ src/main/java/com/algaworks/algafood/api/HostCheckController.java
@@ -6,6 +6,13 @@ import java.net.UnknownHostException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller para retornar o IP e Nome da API
+ * que está respondendo à requisição - Teste de
+ * Poor Man's Load Balancer (DNS Round Robin) 
+ * @author Leonardo
+ *
+ */
 @RestController
 public class HostCheckController {
 
